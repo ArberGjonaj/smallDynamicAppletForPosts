7d51aa89141580ba8348176ae4bad430c397b496
ArberGjonaj <agjonaj@ikubinfo.al>
2021-05-23 22:44:31 +0200

    singup created

--- src/Pages/SingUp.js
+++ src/Pages/SingUp.js
@@ -1,12 +1,111 @@
-import React from 'react';
+import {
+    CButton,
+    CCard,
+    CCardBody,
+    CCardGroup,
+    CCol,
+    CContainer,
+    CForm,
+    CInput,
+    CInputGroup,
+    CInputGroupPrepend,
+    CInputGroupText,
+    CRow,
+  } from "@coreui/react";
+  import CIcon from "@coreui/icons-react";
+import React from "react";
 
-class Register extends React.Component{
+class Register extends React.Component {
+    state = {
+    username: "",
+    password: "",
+  };
+  usernameHandler = (username) => {
+    this.setState({ username: username });
+  };
+  passwordHandler = (password) => {
+    this.setState({ password: password });
+  };
+  login = () => {
+   const redirect=()=>{return this.props.history.push("/dashboard");}
+    iotApi
+      .post("/user_create", {
+        username: this.state.username,
+        password: this.state.password,
+      })
+      .then(function (response) {
+        const token = response.data.token;
+        sessionStorage.setItem("access_token", token);
+        // redirect()
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+  };
+  componentDidMount (){
+
+  }
+
     render(){
-        return(
-            <div>
-                
-            </div>
-        )
-    }
-}
-export default Register;
+    return (
+      <div className="c-app c-default-layout flex-row align-items-center">
+        <CContainer>
+          <CRow className="justify-content-center">
+            <CCol md="8">
+              <CCardGroup>
+                <CCard className="p-4">
+                  <CCardBody>
+                    <CForm>
+                      <h1>Login</h1>
+                      <p className="text-muted">Sign In to your account</p>
+                      <CInputGroup className="mb-3">
+                        <CInputGroupPrepend>
+                          <CInputGroupText>
+                            <CIcon name="cil-user" />
+                          </CInputGroupText>
+                        </CInputGroupPrepend>
+                        <CInput
+                          type="text"
+                          value={this.state.username}
+                          onChange={event=>this.usernameHandler(event.target.value)}
+                          autoComplete="username"
+                        />
+                      </CInputGroup>
+                      <CInputGroup className="mb-4">
+                        <CInputGroupPrepend>
+                          <CInputGroupText>
+                            <CIcon name="cil-lock-locked" />
+                          </CInputGroupText>
+                        </CInputGroupPrepend>
+                        <CInput
+                          type="password"
+                          value={this.state.password}
+                          onChange={event=>this.passwordHandler(event.target.value)}
+                          autoComplete="current-password"
+                        />
+                      </CInputGroup>
+                      <CRow>
+                        <CCol xs="6">
+                          <CButton color="primary" onClick={this.login} className="px-4">
+                            Login
+                          </CButton>
+                        </CCol>
+                        <CCol xs="6" className="text-right">
+                          <CButton color="link" className="px-0">
+                            Forgot password?
+                          </CButton>
+                        </CCol>
+                      </CRow>
+                    </CForm>
+                  </CCardBody>
+                </CCard>
+              </CCardGroup>
+            </CCol>
+          </CRow>
+        </CContainer>
+      </div>
+    );}
+  };
+  
+  export default Register;
